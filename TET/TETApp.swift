44eef30d87029a813ee22803d3cfeb75587c0d5a
MADRAFi <46417792+MADRAFi@users.noreply.github.com>
2022-09-11 18:37:15 +0200

    Initial Commit

---
+++ TET/TETApp.swift
@@ -0,0 +1,17 @@
+//
+//  TETApp.swift
+//  TET
+//
+//  Created by MADRAFi on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
